4d186ded083de9b4855a58e22ac9b45fc1fb724b
Kajal kumari <kajaldewan2023@gmail.com>
2025-04-04 14:57:14 +0530

    aeta

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('Qr Wale', 'Real Victory Groups'),
 
     /*
     |--------------------------------------------------------------------------

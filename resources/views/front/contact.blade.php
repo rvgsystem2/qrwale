a00203650610b8aacc1e62e321260a44c4814b66
chandandigital6 <chandandigital6@gmail.com>
2025-10-08 17:02:08 +0530

    asdfg

--- resources/views/front/contact.blade.php
+++ resources/views/front/contact.blade.php
@@ -1,4 +1,4 @@
-{{-- @include('front.header') --}}
+@include('front.header')
 
     <div class="max-w-6xl mx-auto p-6 my-16">
         <div class="grid grid-cols-1 md:grid-cols-2 rounded-lg overflow-hidden">
